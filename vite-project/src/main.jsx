67a0a22313787cc83f1d4cae9e5ce5795c9fd014
murthy-satti <murthysatti@gmail.com>
2025-12-14 15:04:05 +0530

    winter effect

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Snowfall from 'react-snowfall'
+
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+   <Snowfall
+  // Changes the snowflake color
+  color="white"
+  // Controls the number of snowflakes that are created (default 150)
+  snowflakeCount={200}
+/>
+    
     <App/>
   </StrictMode>,
 )
